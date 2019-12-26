de1e63c949d883dca634d0359c73142995c3cf55
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-26 23:52:12 +0100

    add User model / ctrl / service / repo 🕵️‍♂️ ..

--- src/main/java/tn/imed/jaberi/hospitalmanagement/user/User.java
+++ src/main/java/tn/imed/jaberi/hospitalmanagement/user/User.java
@@ -1,29 +1,115 @@
 package tn.imed.jaberi.hospitalmanagement.user;
 
 
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.annotation.Id;
-
+import java.util.Collection;
 import java.util.Date;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Document(collection = "users")
-public class User {
+public class User implements UserDetails {
 	
 	@Id
 	private String Id;
+	
+    @NotEmpty
     private String email;
+
+    @NotEmpty
     private String name;
+
+    @NotEmpty
+    @JsonIgnore // ignore from response of user .. 
     private String password;
+
     private Date created_at;
     
     
 	public User() {}
     
-    public User(String email, String password, String name) {
+    public User(@NotEmpty String email, @NotEmpty String password, @NotEmpty String name) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.created_at = new Date();
     }
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	public Date getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 
 }
