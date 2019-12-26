9567b8b860cc2866e07801494f9c6a80a2ee13e6
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-26 21:17:32 +0100

    fix ctrl decorator 🎋..

--- src/main/java/tn/imed/jaberi/hospitalmanagement/doctor/DoctorController.java
+++ src/main/java/tn/imed/jaberi/hospitalmanagement/doctor/DoctorController.java
@@ -17,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@RestController
+@RequestMapping("/api/doctors")
 public class DoctorController {
 
 	
