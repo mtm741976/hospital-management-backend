59a4d763caa05d3d97aeb5857142062e5852dc80
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-26 23:10:19 +0100

    add event handler 👨🏻‍🌾 ..

---
+++ src/main/java/tn/imed/jaberi/hospitalmanagement/error/NotFoundException.java
@@ -0,0 +1,17 @@
+package tn.imed.jaberi.hospitalmanagement.error;
+
+
+import org.springframework.http.HttpStatus;
+
+
+public class NotFoundException extends ApiBaseException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    @Override
+    public HttpStatus getStatusCode() {
+        return HttpStatus.NOT_FOUND;
+    }
+}
