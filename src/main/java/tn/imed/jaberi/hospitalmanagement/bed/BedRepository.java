724d4d6d366591742e5744e56654b0a21ecd78bf
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-26 21:04:20 +0100

    fix Repo decorator 🔣 ..

--- src/main/java/tn/imed/jaberi/hospitalmanagement/bed/BedRepository.java
+++ src/main/java/tn/imed/jaberi/hospitalmanagement/bed/BedRepository.java
@@ -1,7 +1,9 @@
 package tn.imed.jaberi.hospitalmanagement.bed;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BedRepository extends MongoRepository<Bed, String> {
 
 }
